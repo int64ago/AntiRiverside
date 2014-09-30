587e1f5085c540a5e6453ae56f5557f7eb746afb
Cody Chan <int64ago@gmail.com>
2014-09-30 18:04:40 +0800

    block the default fucking signature

--- js/content/filter.js
+++ js/content/filter.js
@@ -3,7 +3,9 @@ function filterSignature(blackList){
 	$("#postlist > div[class!='pl']").each(function(){
 		var pls = $(this).find(".pls"); // author info
 		var plc = $(this).find(".plc"); // post info
-		if($.inArray(pls.find(".xw1").text(), blackList) != -1){
+		var rapeUser = "请阅读新手导航";
+		if($.inArray(pls.find(".xw1").text(), blackList) != -1 ||
+			$(this).find(".sign").text().indexOf(rapeUser) != -1){
 			$(this).find(".sign").attr({"style": "display:none;"});
 		}
 	});
